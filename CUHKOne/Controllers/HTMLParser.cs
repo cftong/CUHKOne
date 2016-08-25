3ad907d22bc961921dfb3c3ceb574a51ce638ce2
TONG Cham Fei <cftong@outlook.com>
2016-08-25 16:49:50 +0800

    Initial Commit

---
+++ CUHKOne/Controllers/HTMLParser.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+
+namespace CUHKOne.Controllers
+{
+  public class HTMLParser
+  {
+    public HtmlNodeCollection parser(string addr)
+    {
+      HtmlDocument document = new HtmlDocument();
+      document.LoadHtml("http://cumassmail.itsc.cuhk.edu.hk/weekly/Digest/List/UG/20160812");
+      HtmlNodeCollection collection = document.DocumentNode.SelectNodes("//*[contains(@class,'float')]");
+      return collection;
+    }
+  }
+}
